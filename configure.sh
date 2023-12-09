3e961a515008f0350c6b1ff6945c9700fd83c281
artofnothingness <budyakov.aleksei@gmail.com>
2023-12-09 15:27:34 +0300

    upd

--- configure.sh
+++ configure.sh
@@ -3,20 +3,6 @@
 sudo apt update
 sudo apt install -y software-properties-common stow
 
-install_regolith() {
-  wget -qO - https://regolith-desktop.org/regolith.key | \
-    gpg --dearmor | sudo tee /usr/share/keyrings/regolith-archive-keyring.gpg > /dev/null
-
-  echo deb "[arch=amd64 signed-by=/usr/share/keyrings/regolith-archive-keyring.gpg] \
-    https://regolith-desktop.org/release-ubuntu-jammy-amd64 jammy main" | \
-    sudo tee /etc/apt/sources.list.d/regolith.list
-
-  sudo apt update
-  sudo apt install regolith-desktop regolith-compositor-picom-glx
-  sudo apt install -y i3xrocks-focused-window-name i3xrocks-rofication i3xrocks-info i3xrocks-app-launcher i3xrocks-memory
-  sudo apt upgrade
-}
-
 install_zsh() {
   git clone --depth=1 git@github.com:ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
   git clone --depth=1 git@github.com:romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
@@ -26,28 +12,14 @@ install_zsh() {
   sudo apt install -y zsh autojump
 }
 
-install_nvim() {
-  sudo apt install -y \
-    npm 
-
-  git clone --depth=1 https://github.com/artofnothingness/nvim.git ~/.config/nvim
-  source ~/.config/nvim/utils/install.sh
-}
-
 install_tools() {
-  echo 'deb http://download.opensuse.org/repositories/home:/stig124:/nnn/xUbuntu_20.04/ /' | sudo tee /etc/apt/sources.list.d/home:stig124:nnn.list
-  curl -fsSL https://download.opensuse.org/repositories/home:stig124:nnn/xUbuntu_20.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_stig124_nnn.gpg > /dev/null
-
   sudo apt update
   sudo apt install -y \
     zsh \
     neofetch \
     fzf \
-    fonts-font-awesome \
     lm-sensors \
-    nnn \
     tmux \
-    tmuxinator \
     autojump \
     python3-pip
 
@@ -62,10 +34,6 @@ do
         --tools) install_tools
             ;;
         --zsh) install_zsh
-            ;;
-        --nvim) install_nvim
-            ;;
-        --regolith) install_regolith
             ;;
         --*) echo "Bad option $1"
             ;;
